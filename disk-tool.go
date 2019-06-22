60c8f9677c4747c791dd34ee68c3f3126dde6781
Elliott Newton <elliottnewton8@gmail.com>
2019-06-22 17:27:12 +0100

    add disk fill predictions, add stats links

--- disk-tool.go
+++ disk-tool.go
@@ -18,25 +18,28 @@ import (
 )
 
 type disk struct {
-	Mount             string
-	MountEscaped      string
-	Size              uint64
-	SizeReadable      string
-	Available         uint64
-	AvailableReadable string
-	Used              uint64
-	UsedReadable      string
-	Percentage        uint8
+	Mount             string // directory to check
+	MountEscaped      string // escaped sirectory (no slashes)
+	Size              uint64 // size of disk in bytes
+	SizeReadable      string // size of disk in readable format
+	Available         uint64 // available disk in bytes
+	AvailableReadable string // availble disk in readable format
+	Used              uint64 // used disk in bytes
+	UsedReadable      string // used disk in readable format
+	Percentage        uint8  // percentage of disk used
 }
 
 type statsInfo struct {
-	Disk          disk
-	ChangeMonth   string
-	MonthPositive bool
-	ChangeWeek    string
-	WeekPositive  bool
-	ChangeDay     string
-	DayPositive   bool
+	Disk          disk   // disk the stats are for
+	ChangeMonth   string // change in disk usage past 30 days
+	MonthPositive bool   // increase or decrease
+	FullMonth     string // how long till disk full calculated from ChangeMonth
+	ChangeWeek    string // change in disk usage past 7 days
+	WeekPositive  bool   // increase or decrease
+	FullWeek      string // how long till disk full calculated from ChangeWeek
+	ChangeDay     string // change in disk usage past 24 hours
+	DayPositive   bool   // increase or decrease
+	FullDay       string // how long till disk full calculated from ChangeDay
 }
 
 var db *sql.DB
@@ -61,6 +64,7 @@ func infoHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusBadRequest)
 }
 
+// serves sql data as json array
 func dataHandler(w http.ResponseWriter, r *http.Request) {
 	diskName := strings.Replace(r.URL.Path[len("/data/"):], "@", "/", -1)
 	for _, disk := range diskInfo {
@@ -104,36 +108,67 @@ func main() {
 	http.ListenAndServe(":8192", nil)
 }
 
+// calculates statistics from current and past disk info
 func getStatsInfo(disk disk) statsInfo {
 	var times []int64
 
 	timeNow := time.Now().Unix()
-	times = append(times, timeNow-2592000)
-	times = append(times, timeNow-604800)
-	times = append(times, timeNow-86400)
+	times = append(times, timeNow-2592000) // 30 days ago
+	times = append(times, timeNow-604800)  // 7 days ago
+	times = append(times, timeNow-86400)   // 1 day ago
 
-	var results []string
-	var changeValue []bool
+	var results []string     // disk space change - readable
+	var changeValue []bool   // is change positive or negative?
+	var predictions []string // when disk space will run out based on different changes
 
-	for _, time := range times {
-		query := fmt.Sprintf("SELECT bytes FROM logs WHERE time > %s AND mount='%s' ORDER BY time ASC LIMIT 1", strconv.FormatInt(time, 10), disk.Mount)
+	for _, t := range times {
+		// get the first log after the given time for the given disk
+		query := fmt.Sprintf("SELECT bytes FROM logs WHERE time > %s AND mount='%s' ORDER BY time ASC LIMIT 1", strconv.FormatInt(t, 10), disk.Mount)
 		var bytes uint64
 		err := db.QueryRow(query).Scan(&bytes)
+
 		if err != nil {
 			results = append(results, "ERROR")
 			changeValue = append(changeValue, false)
+			predictions = append(predictions, "N/A")
 		} else {
-			if disk.Available > bytes {
+			if disk.Available >= bytes {
 				results = append(results, humanize.IBytes(disk.Available-bytes))
 				changeValue = append(changeValue, true)
+				predictions = append(predictions, "N/A")
 			} else {
 				results = append(results, "-"+humanize.IBytes(bytes-disk.Available))
 				changeValue = append(changeValue, false)
+
+				spaceChange := int64(bytes - disk.Available)              // disk space difference
+				timeChange := timeNow - t                                 // time difference
+				bytesPerSecond := spaceChange / timeChange                // bytes used per second on the disk
+				secondsTillFull := int64(disk.Available) / bytesPerSecond // seconds until disk is full at current rate
+
+				// parses the number of seconds into a more human readable time format
+				var timeTillFull string
+				if secondsTillFull > 31556952 {
+					timeTillFull = strconv.FormatInt(secondsTillFull/31556952, 10)
+					if secondsTillFull > 31556952*2 {
+						timeTillFull += " years"
+					} else {
+						timeTillFull += " year"
+					}
+				} else {
+					timeTillFull = strconv.FormatInt(secondsTillFull/86400, 10)
+					if secondsTillFull > 86400*2 {
+						timeTillFull += " days"
+					} else {
+						timeTillFull += " day"
+					}
+				}
+
+				predictions = append(predictions, timeTillFull)
 			}
 		}
 	}
 
-	s := statsInfo{disk, results[0], changeValue[0], results[1], changeValue[1], results[2], changeValue[2]}
+	s := statsInfo{disk, results[0], changeValue[0], predictions[0], results[1], changeValue[1], predictions[1], results[2], changeValue[2], predictions[2]}
 	return s
 }
 
@@ -145,23 +180,24 @@ func updateDiskInfo(watchList []string) {
 		mountEscaped := strings.Replace(dir, "/", "@", -1)
 
 		var stat syscall.Statfs_t
-		syscall.Statfs(dir, &stat)
+		syscall.Statfs(dir, &stat) // get directory info - LINUX ONLY
 
-		bSize := uint64(stat.Bsize)
+		bSize := uint64(stat.Bsize) // block size
 
-		size := uint64(stat.Blocks * bSize)
-		sizeReadable := humanize.IBytes(size)
-		available := uint64(stat.Bavail * bSize)
-		availableReadable := humanize.IBytes(available)
-		used := size - available
-		usedReadable := humanize.IBytes(used)
-		percentage := uint8(math.Round((float64(used) / float64(size)) * 100))
+		size := uint64(stat.Blocks * bSize)                                    // disk size
+		sizeReadable := humanize.IBytes(size)                                  // readable disk size
+		available := uint64(stat.Bavail * bSize)                               // availible space on disk
+		availableReadable := humanize.IBytes(available)                        // readable availble space
+		used := size - available                                               // used disk space
+		usedReadable := humanize.IBytes(used)                                  // readable used disk space
+		percentage := uint8(math.Round((float64(used) / float64(size)) * 100)) // percentage of disk space used
 
 		dInfo = append(dInfo, disk{dir, mountEscaped, size, sizeReadable, available, availableReadable, used, usedReadable, percentage})
 	}
 	diskInfo = dInfo
 }
 
+// periodically records disk usage and current time to database
 func logDiskInfo(logInterval int, watchList []string) {
 	// make sure the log table exists
 	_, err := db.Exec("CREATE TABLE IF NOT EXISTS logs (mount VARCHAR(100), time TIMESTAMP, bytes BIGINT)")
